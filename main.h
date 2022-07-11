c5e0972f6ac4626f2d4ef587d9b97b3a9193e303
Maykha27 <5144@holbertonschool.com>
2022-07-11 13:12:30 +0100

    print

--- main.h
+++ main.h
@@ -23,14 +23,6 @@ int print_s(va_list str);
 int print_c(va_list ch);
 int print_i(va_list intg);
 int print_d(va_list dec);
-int print_u(va_list u);
-int print_b(va_list b);
-int print_o(va_list o);
-int print_x(va_list x);
-int print_X(va_list X);
-int print_p(va_list p);
-int print_S(va_list S);
-int print_r(va_list r);
-int print_R(va_list R);
+
 
 #endif
